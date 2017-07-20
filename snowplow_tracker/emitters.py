486f4af6016b37231118732846201722f255cc3a
Jonathan Neddenriep <river1@gmail.com>
2017-07-20 14:46:07 -0700

    Remove flush on app.

--- snowplow_tracker/emitters.py
+++ snowplow_tracker/emitters.py
@@ -161,7 +161,6 @@ class Emitter(object):
         else:
             return self.bytes_queued >= self.byte_limit or len(self.buffer) >= self.buffer_size
 
-    @app(name="Flush")
     def flush(self):
         """
             Sends all events in the buffer to the collector.
